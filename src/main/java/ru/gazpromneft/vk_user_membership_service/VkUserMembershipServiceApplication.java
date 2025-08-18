4bd63c70a40b8167aeaf67ccbe0c4d35f0e4d829
sokolov_ii@crc.ru <ilyasokolov63@mail.ru>
2025-08-18 17:00:04 +0300

    initial commit

---
+++ src/main/java/ru/gazpromneft/vk_user_membership_service/VkUserMembershipServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.gazpromneft.vk_user_membership_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkUserMembershipServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkUserMembershipServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gazpromneft/vk_user_membership_service/VkUserMembershipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gazpromneft.vk_user_membership_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkUserMembershipServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
